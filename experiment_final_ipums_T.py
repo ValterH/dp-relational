749faf41042cc4061c2a7fc49925f92e64a7e721
Ojas Gulati <ojas.gulati@gmail.com>
2024-05-18 16:59:32 -0400

    first ipums experiment!

--- experiment_final_ipums_T.py
+++ experiment_final_ipums_T.py
@@ -20,27 +20,34 @@ def print_iter_eval(qm, b_round, T):
 def qm_generator_torch(rel_dataset, k, df1_synth, df2_synth):
     return dp_relational.lib.synth_data.QueryManagerTorch(rel_dataset, k=k, df1_synth=df1_synth, df2_synth=df2_synth, device=device)
 
+""" Small IPUMS tables """
+
+fraction = 0.05
+table_size = 1000
+
 def cross_generator_torch(qm, eps_rel, T):
     b_round = dp_relational.lib.synth_data.learn_relationship_vector_torch_paper_algo(qm, eps_rel, T=T,
-                subtable_size=100000, verbose=True, device=device, queries_to_reuse=8)
+                subtable_size=(table_size * table_size), verbose=True, device=device, queries_to_reuse=None,
+                exp_mech_alpha=0.2
+                )
     relationship_syn = dp_relational.lib.synth_data.make_synthetic_rel_table_sparse(qm, b_round)
     return relationship_syn
 
-runner = ModelRunner(self_relation=True)
-runner.update(dataset_generator=dp_relational.data.ipums, n_syn1=10000, n_syn2=10000,
-              synth='mst', epsilon=4.0, eps1=1.0, eps2=1.0, k=3, dmax=10,
-              qm_generator=qm_generator_torch, cross_generation_strategy=cross_generator_torch,
-              T=60)
-runner.load_artifacts('0c96cc62-014b-11ef-9020-d21cd07a44f3')
+runner = ModelRunner()
 
-Ts = [0, 5, 10, 15, 20, 25, 30]
+runner.update(dataset_generator=lambda dmax: dp_relational.data.ipums.dataset(dmax, frac=0.05), n_syn1=table_size, n_syn2=table_size,
+              synth='mst', epsilon=4.0, eps1=1.0, eps2=1.0, k=3, dmax=4,
+              qm_generator=qm_generator_torch, cross_generation_strategy=cross_generator_torch)
+
+Ts = [0, 5, 10, 15, 20]
 run_count = 0
 while True:
-    for T_num in Ts:
-        runner.update(T=T_num)
+    for T in Ts:
+        runner.update(T=T)
         runner.regenerate_qm = True
-        results = runner.run(extra_params={ "run_set": 1101 })
+        results = runner.run(extra_params={ "run_set": 2000, "alpha": 0.2 })
+        print(runner.rel_dataset_runid)
         print(runner.relationship_syn.shape[0])
         run_count += 1
-        print(f"T: {T_num}, error_ave: {results['error_ave']}")
+        print(f"T: {T}, error_ave: {results['error_ave']}")
         print(f"###### COMPLETED {run_count} RUNS ######")
