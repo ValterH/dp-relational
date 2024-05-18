0b241154b6318f07062db2a945c87113760b9c58
Ojas Gulati <ojas.gulati@gmail.com>
2024-05-18 05:56:15 -0400

    added alpha expaeriment

--- experiment_final_fix_alpha.py
+++ experiment_final_fix_alpha.py
@@ -26,7 +26,7 @@ runner = ModelRunner()
 runner.update(dataset_generator=dp_relational.data.movies.dataset, n_syn1=3880, n_syn2=6040,
               synth='mst', epsilon=4.0, eps1=1.0, eps2=1.0, k=3, dmax=10,
               qm_generator=qm_generator_torch, cross_generation_strategy=cross_generator_torch,
-              T=5)
+              T=110)
 runner.load_artifacts('0c96cc62-014b-11ef-9020-d21cd07a44f3')
 print("hiii!")
 alphas = [0.000001, 0.1, 0.2, 0.3, 0.4]
@@ -36,7 +36,7 @@ while True:
         alpha_value = alpha
         runner.regenerate_qm = True
         runner.regenerate_cross_answers = True
-        results = runner.run(extra_params={ "run_set": "final_fix_reusing2_with_expmech_3_test", "alpha": alpha_value })
+        results = runner.run(extra_params={ "run_set": "final_fix_reusing2_with_expmech_4", "alpha": alpha_value })
         print(runner.relationship_syn.shape[0])
         run_count += 1
         print(f"alpha: {alpha_value}, error_ave: {results['error_ave']}")
