bfa3109801365c7482e9537e0904a6307c7bfa77
ValterH <valter.hudovernik@gmail.com>
2025-07-29 12:05:29 -0700

    support airbnb generation

--- dp_relational/data/airbnb.py
+++ dp_relational/data/airbnb.py
@@ -1,3 +1,7 @@
+import json
+
+import numpy as np
+
 from pathlib import Path
 from syntherela.data import load_tables
 from syntherela.metadata import Metadata
@@ -5,6 +9,14 @@ from dp_relational.lib.dataset import Table, RelationalDataset
 
 
 data_path = "data/original/airbnb-simplified_subsampled"
+
+
+def fill_nans(df, col):
+    if df[col].isnull().any():
+        col_mean = df[col].mean()
+        df[col + "_missing"] = df[col].isnull().astype(int)
+        df[col] = df[col].fillna(col_mean)
+    return df
 
 
 def dataset(dmax):
@@ -15,22 +27,74 @@ def dataset(dmax):
     sessions_df = tables["sessions"]
 
     categorical_columns_users = metadata.get_column_names("users", sdtype="categorical")
+    categorical_columns_sessions = metadata.get_column_names(
+        "sessions", sdtype="categorical"
+    )
+    datetime_columns_users = metadata.get_column_names("users", sdtype="datetime")
+
+    # convert datetime columns to integer timestamps
+    for col in datetime_columns_users:
+        users_df[col] = users_df[col].astype("int64")
 
     # convert categorical columns to strings
     for col in categorical_columns_users:
         users_df[col] = users_df[col].astype(str)
-        # # fill NaN values with "unknown"
-        # users_df[col] = users_df[col].fillna("unknown")
-
-    categorical_columns_sessions = metadata.get_column_names(
-        "sessions", sdtype="categorical"
-    )
 
     # convert categorical columns to strings
     for col in categorical_columns_sessions:
         sessions_df[col] = sessions_df[col].astype(str)
-        # # fill NaN values with "unknown"
-        # sessions_df[col] = sessions_df[col].fillna("unknown")
+
+    numeric_columns_users = metadata.get_column_names("users", sdtype="numerical")
+    numeric_columns_sessions = metadata.get_column_names("sessions", sdtype="numerical")
+
+    numeric_columns_users += datetime_columns_users
+
+    bins = {
+        "users": {},
+        "sessions": {},
+    }
+
+    # fill nan values and create boolean columns for missing values
+    for col in numeric_columns_users:
+        if users_df[col].isnull().any():
+            users_df = fill_nans(users_df, col)
+
+        if len(users_df[col].unique()) > 5:
+            # discretize the data into 5 bins
+            bins["users"][col] = np.linspace(
+                users_df[col].min(), users_df[col].max(), 6
+            ).tolist()
+            users_df[col] = (
+                np.digitize(
+                    users_df[col],
+                    bins=bins["users"][col],
+                )
+                - 1
+            )
+
+    for col in numeric_columns_sessions:
+        if sessions_df[col].isnull().any():
+            sessions_df = fill_nans(sessions_df, col)
+        if sessions_df[col].isnull().any():
+            sessions_df = fill_nans(sessions_df, col)
+
+        if len(sessions_df[col].unique()) > 5:
+            # discretize the data into 5 bins
+            bins["sessions"][col] = np.linspace(
+                sessions_df[col].min(), sessions_df[col].max(), 6
+            ).tolist()
+            sessions_df[col] = (
+                np.digitize(
+                    sessions_df[col],
+                    bins=bins["sessions"][col],
+                )
+                - 1
+            )
+        else:
+            pass
+
+    with open(Path(data_path) / "bins.json", "w") as f:
+        json.dump(bins, f, indent=4)
 
     pk_users = metadata.get_primary_key("users")
     # sessions does not have a primary key, so we create one
